52372f07f6f28ceb2be76bb9483706ad2aa8a76e
0VERRIDER <anshilp@karunya.edu.in>
2021-12-16 07:49:35 +0530

    Layout updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Logo from '../public/id_verification.png'
 
 export default function Home() {
   return (
@@ -15,7 +16,9 @@ export default function Home() {
         <button className={styles.registerBtn}>Register</button>
      </div>
      <div className={styles.subMain}>
-       
+       <div className={styles.student_id} >
+       <Image src={Logo} alt="student id logo" className={styles.studentId} width={2} height={2} layout='responsive'></Image>
+       </div>
      </div>
 
     </div>
